4ff52239a50a0624424e3dac87855a2a9b65cffb
tauheed0110 <tauheed0110@gmail.com>
2024-11-16 09:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const password = document.getElementById('password');
 const checkbox = document.getElementById('checkbox');
 const submit = document.getElementById('submit');
 const existingUserBtn = document.getElementById('existing');
+const main = document.getElementById('main');
+
 
 submit.addEventListener('click', (e)=>{
     // if the checkbox is checked save the data to local storage and alert
@@ -15,20 +17,19 @@ submit.addEventListener('click', (e)=>{
     else if(checkbox.checked){
         localStorage.setItem("username", JSON.stringify(username.value));
         localStorage.setItem("password", JSON.stringify(password.value));
+        main.style.display='none';
+        existingUserBtn.style.display='block';
+        location.reload();
     }
     alert(`Logged in as ${username.value}`)
-    location.reload();
 })
 
 if(localStorage.getItem('username')){
-    const btn = document.createElement('button');
-    btn.setAttribute('id', 'existing');
-    btn.textContent = 'Login as existing user';
-    document.body.innerHTML = '';
-    document.body.appendChild(btn);
-
+    
+    main.style.display='none';
+    existingUserBtn.style.display='block';
     // handle any button click.
-    btn.addEventListener('click', ()=>{
+    existingUserBtn.addEventListener('click', ()=>{
         const user = JSON.parse(localStorage.getItem('username'));
         alert(`Logged in as ${user}`);
     })
